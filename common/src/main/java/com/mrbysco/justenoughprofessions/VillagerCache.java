6f2b60c8c55ee8154d42a0c3e154238cf5609f30
Mrbysco <Mrbysco@users.noreply.github.com>
2024-09-06 20:35:09 +0200

    Only use a one villager instance

---
+++ common/src/main/java/com/mrbysco/justenoughprofessions/VillagerCache.java
@@ -0,0 +1,39 @@
+package com.mrbysco.justenoughprofessions;
+
+import com.mrbysco.justenoughprofessions.platform.Services;
+import net.minecraft.client.Minecraft;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+public class VillagerCache {
+	private static Villager cachedVillager;
+
+	@Nullable
+	public static Villager getVillagerEntity(VillagerProfession profession) {
+		if (cachedVillager == null) {
+			CompoundTag nbt = new CompoundTag();
+			nbt.putString("id", Objects.requireNonNull(Services.PLATFORM.getEntityKey(EntityType.VILLAGER)).toString());
+			Minecraft mc = Minecraft.getInstance();
+			Level level = mc.hasSingleplayerServer() && mc.getSingleplayerServer() != null ? mc.getSingleplayerServer().getAllLevels().iterator().next() : mc.level;
+			if (level != null) {
+				Villager villager = (Villager) EntityType.loadEntityRecursive(nbt, level, Function.identity());
+				if (villager != null) {
+					cachedVillager = villager;
+				}
+			}
+		} else {
+			cachedVillager.setVillagerData(cachedVillager.getVillagerData().setProfession(profession));
+			return cachedVillager;
+		}
+
+		return null;
+	}
+
+}
--- common/src/main/java/com/mrbysco/justenoughprofessions/jei/ProfessionEntry.java
+++ common/src/main/java/com/mrbysco/justenoughprofessions/jei/ProfessionEntry.java
@@ -1,18 +1,12 @@
 package com.mrbysco.justenoughprofessions.jei;
 
-import com.mrbysco.justenoughprofessions.platform.Services;
-import net.minecraft.client.Minecraft;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.entity.EntityType;
+import com.mrbysco.justenoughprofessions.VillagerCache;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.function.Function;
 
 /**
  * A record to hold the profession and the block stacks for the profession
@@ -27,17 +21,6 @@ public record ProfessionEntry(VillagerProfession profession, List<ItemStack> blo
 	 */
 	@Nullable
 	public Villager getVillagerEntity() {
-		CompoundTag nbt = new CompoundTag();
-		nbt.putString("id", Objects.requireNonNull(Services.PLATFORM.getEntityKey(EntityType.VILLAGER)).toString());
-		Minecraft mc = Minecraft.getInstance();
-		Level level = mc.hasSingleplayerServer() && mc.getSingleplayerServer() != null ? mc.getSingleplayerServer().getAllLevels().iterator().next() : mc.level;
-		if (level != null) {
-			Villager villager = (Villager) EntityType.loadEntityRecursive(nbt, level, Function.identity());
-			if (villager != null) {
-				villager.setVillagerData(villager.getVillagerData().setProfession(this.profession));
-				return villager;
-			}
-		}
-		return null;
+		return VillagerCache.getVillagerEntity(this.profession);
 	}
 }
